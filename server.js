6d405e6c733e5359f82abf90301c0632ff202470
SilverPlains <bob@silverplainselectric.com>
2026-05-07 22:03:04 -0600

    Switched to secure SSL port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
-  secure: false, // true for 465, false for other ports
+  secure: true, // MUST be true for port 465
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
